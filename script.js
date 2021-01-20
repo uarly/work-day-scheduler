db9d08979525183c82e37a52708b86d576e00bff
Arly U <arlyu@Arlys-Air.lan>
2021-01-19 21:41:27 -0600

    local storage state item working

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ $(".btn").on("click",function(){
     var timeblock = $(this).attr("id").split("-")[0]
     var userentry = $("#"+timeblock+"-txt").val()
     console.log(timeblock,userentry)
+    localStorage.setItem(timeblock,userentry)
 })
