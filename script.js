d7c301f53f6cdef0452edcd61d8c797c91fd1746
Arly U <arlyu@Arlys-Air.lan>
2021-01-25 16:39:01 -0600

    scriptjs

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ $(".btn").on("click",function(){
     console.log(timeblock,userentry)
     localStorage.setItem(timeblock,userentry)
 })
+var today = moment()
+var hour = moment().hours()
+console.log(today,hour)
+$("#currentDay").text(today)
+for(let i=9;i<18;i++){
+    $("#"+i+"-txt").val(localStorage.getItem(i))
+    if ( i > hour){
+        $("#"+i+"-txt").addClass("bg-success")
+    }else if(i == hour){
+        $("#"+i+"-txt").addClass("bg-info")
+    }
+    else {
+        $("#"+i+"-txt").addClass("bg-secondary")
+    }
+}
